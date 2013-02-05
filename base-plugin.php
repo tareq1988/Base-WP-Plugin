c274e5602408439b0280b447eebb8898018e7992
Tareq Hasan <tareq1988@gmail.com>
2013-02-06 02:06:21 +0600

    tabs converted to spaces, because I PREFER

--- base-plugin.php
+++ base-plugin.php
@@ -11,19 +11,19 @@ License: GPL2
 
 // don't call the file directly
 if ( ! defined( 'ABSPATH' ) )
-	return;
+    return;
 
 $baseplugin_file = __FILE__;
 
 /* Find our plugin, wherever it may live! */
 if ( isset( $plugin ) ) {
-	$baseplugin_file = $plugin;
+    $baseplugin_file = $plugin;
 }
 else if ( isset( $mu_plugin ) ) {
-	$baseplugin_file = $mu_plugin;
+    $baseplugin_file = $mu_plugin;
 }
 else if ( isset( $network_plugin ) ) {
-	$baseplugin_file = $network_plugin;
+    $baseplugin_file = $network_plugin;
 }
 
 define( 'BASEPLUGIN_FILE', $baseplugin_file );
@@ -32,191 +32,191 @@ define( 'BASEPLUGIN_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( $baseplugin_
 /**
  * Base_Plugin class
  *
- * @class Base_Plugin	The class that holds the entire Base_Plugin plugin
+ * @class Base_Plugin   The class that holds the entire Base_Plugin plugin
  */
 class Base_Plugin {
 
-	/**
-	 * @var $name	Variable for Base_Plugin used throughout the plugin
-	 */
-	protected $name = "Base Plugin";
+    /**
+     * @var $name   Variable for Base_Plugin used throughout the plugin
+     */
+    protected $name = "Base Plugin";
 
-	/**
-	 * @var $nonce_key	A security key used internally by the plugin
-	 */
-	protected $nonce_key = '+Y|*Ec/-\s3';
+    /**
+     * @var $nonce_key  A security key used internally by the plugin
+     */
+    protected $nonce_key = '+Y|*Ec/-\s3';
 
-	/**
-	 * PHP 5.3 and lower compatibility
-	 *
-	 * @uses Base_Plugin::__construct()
-	 *
-	 */
-	public function Base_Plugin() {
-		$this->__construct();
-	}
+    /**
+     * PHP 5.3 and lower compatibility
+     *
+     * @uses Base_Plugin::__construct()
+     *
+     */
+    public function Base_Plugin() {
+        $this->__construct();
+    }
 
-	/**
-	 * Constructor for the Base_Plugin class
-	 *
-	 * Sets up all the appropriate hooks and actions
-	 * within our plugin.
-	 *
-	 * @uses register_activation_hook()
-	 * @uses register_deactivation_hook()
-	 * @uses is_admin()
-	 * @uses add_action()
-	 *
-	 */
-	public function __construct() {
-		register_activation_hook( BASEPLUGIN_FILE, array( &$this, 'activate' ) );
-		register_deactivation_hook( BASEPLUGIN_FILE, array( &$this, 'deactivate' ) );
+    /**
+     * Constructor for the Base_Plugin class
+     *
+     * Sets up all the appropriate hooks and actions
+     * within our plugin.
+     *
+     * @uses register_activation_hook()
+     * @uses register_deactivation_hook()
+     * @uses is_admin()
+     * @uses add_action()
+     *
+     */
+    public function __construct() {
+        register_activation_hook( BASEPLUGIN_FILE, array( &$this, 'activate' ) );
+        register_deactivation_hook( BASEPLUGIN_FILE, array( &$this, 'deactivate' ) );
 
-		// Localize our plugin
-		add_action( 'init', array( &$this,'localization_setup' ) );
+        // Localize our plugin
+        add_action( 'init', array( &$this,'localization_setup' ) );
 
-		// Set up admin-specific scripts
-		add_action( 'admin_menu', array( $this,'menu_setup' ) );
+        // Set up admin-specific scripts
+        add_action( 'admin_menu', array( $this,'menu_setup' ) );
 
-	}
+    }
 
-	/**
-	 * Initializes the Base_Plugin() class
-	 *
-	 * Checks for an existing Base_Plugin() instance
-	 * and if it doesn't find one, creates it.
-	 *
-	 * @uses Base_Plugin()
-	 *
-	 */
-	public function &init() {
-		static $instance = false;
+    /**
+     * Initializes the Base_Plugin() class
+     *
+     * Checks for an existing Base_Plugin() instance
+     * and if it doesn't find one, creates it.
+     *
+     * @uses Base_Plugin()
+     *
+     */
+    public function &init() {
+        static $instance = false;
 
-		if ( ! $instance ) {
-			$instance = new Base_Plugin();
-		}
+        if ( ! $instance ) {
+            $instance = new Base_Plugin();
+        }
 
-		return $instance;
-	}
+        return $instance;
+    }
 
-	/**
-	 * Placeholder for activation function
-	 *
-	 * Nothing being called here yet.
-	 */
-	public function activate() {
+    /**
+     * Placeholder for activation function
+     *
+     * Nothing being called here yet.
+     */
+    public function activate() {
 
-	}
+    }
 
-	/**
-	 * Placeholder for deactivation function
-	 *
-	 * Nothing being called here yet.
-	 */
-	public function deactivate() {
+    /**
+     * Placeholder for deactivation function
+     *
+     * Nothing being called here yet.
+     */
+    public function deactivate() {
 
-	}
+    }
 
-	/**
-	 * Initialize plugin for localization
-	 *
-	 * @uses load_plugin_textdomain()
-	 *
-	 */
-	public function localization_setup() {
-		load_plugin_textdomain( 'baseplugin', false, dirname( plugin_basename( BASEPLUGIN_FILE ) ) . '/languages/' );
-	}
+    /**
+     * Initialize plugin for localization
+     *
+     * @uses load_plugin_textdomain()
+     *
+     */
+    public function localization_setup() {
+        load_plugin_textdomain( 'baseplugin', false, dirname( plugin_basename( BASEPLUGIN_FILE ) ) . '/languages/' );
+    }
 
-	/**
-	 * Creates admin page and enqueues our admin_scripts hook
-	 *
-	 * @uses add_menu_page()
-	 * @uses add_action()
-	 *
-	 */
-	public function menu_setup() {
-		$hook = add_menu_page( $this->name, $this->name, 'manage_options', 'baseplugin', array(&$this, 'plugin_page_settings' ), plugins_url( 'images/menu.png', BASEPLUGIN_FILE ) );
-		add_action( 'admin_print_styles-' . $hook, array( $this, 'admin_styles' ) );
-		add_action( 'admin_print_scripts-' . $hook, array( $this, 'admin_scripts' ) );
-	}
+    /**
+     * Creates admin page and enqueues our admin_scripts hook
+     *
+     * @uses add_menu_page()
+     * @uses add_action()
+     *
+     */
+    public function menu_setup() {
+        $hook = add_menu_page( $this->name, $this->name, 'manage_options', 'baseplugin', array(&$this, 'plugin_page_settings' ), plugins_url( 'images/menu.png', BASEPLUGIN_FILE ) );
+        add_action( 'admin_print_styles-' . $hook, array( $this, 'admin_styles' ) );
+        add_action( 'admin_print_scripts-' . $hook, array( $this, 'admin_scripts' ) );
+    }
 
-	/**
-	 * Enqueue admin scripts
-	 *
-	 * Allows plugin assets to be loaded.
-	 *
-	 * @uses wp_enqueue_script()
-	 * @uses wp_localize_script()
-	 *
-	 */
-	public function admin_scripts() {
+    /**
+     * Enqueue admin scripts
+     *
+     * Allows plugin assets to be loaded.
+     *
+     * @uses wp_enqueue_script()
+     * @uses wp_localize_script()
+     *
+     */
+    public function admin_scripts() {
 
-		/**
-		 * Example for enqueueing our plugin styles
-		 *
-		 * Uncomment line below and replace with assets specific to your plugin.
-		 */
-		wp_enqueue_script( 'baseplugin-scripts', plugins_url( 'scripts/baseplugin.js', BASEPLUGIN_FILE ), array( 'jquery' ), date( 'Ymd' ) );
+        /**
+         * Example for enqueueing our plugin styles
+         *
+         * Uncomment line below and replace with assets specific to your plugin.
+         */
+        wp_enqueue_script( 'baseplugin-scripts', plugins_url( 'scripts/baseplugin.js', BASEPLUGIN_FILE ), array( 'jquery' ), date( 'Ymd' ) );
 
 
-		/**
-		 * Example for setting up text strings from Javascript files for localization
-		 *
-		 * Uncomment line below and replace with proper localization variables.
-		 */
-		// $translation_array = array( 'some_string' => __( 'Some string to translate', 'baseplugin' ), 'a_value' => '10' );
-		// wp_localize_script( 'base-plugin-scripts', 'baseplugin', $translation_array ) );
+        /**
+         * Example for setting up text strings from Javascript files for localization
+         *
+         * Uncomment line below and replace with proper localization variables.
+         */
+        // $translation_array = array( 'some_string' => __( 'Some string to translate', 'baseplugin' ), 'a_value' => '10' );
+        // wp_localize_script( 'base-plugin-scripts', 'baseplugin', $translation_array ) );
 
-	}
+    }
 
-	/**
-	 * Enqueue admin styles
-	 *
-	 * Allows plugin assets to be loaded.
-	 *
-	 * @uses wp_enqueue_style()
-	 *
-	 */
-	public function admin_styles() {
+    /**
+     * Enqueue admin styles
+     *
+     * Allows plugin assets to be loaded.
+     *
+     * @uses wp_enqueue_style()
+     *
+     */
+    public function admin_styles() {
 
-		/*
-		* Example for enqueueing our plugin styles
-		*
-		* Uncomment line below and replace with assets specific to your plugin.
-		*/
+        /*
+        * Example for enqueueing our plugin styles
+        *
+        * Uncomment line below and replace with assets specific to your plugin.
+        */
 
-		wp_enqueue_style( 'baseplugin-styles', plugins_url( 'styles/baseplugin.css', BASEPLUGIN_FILE ), false, date( 'Ymd' ) );
+        wp_enqueue_style( 'baseplugin-styles', plugins_url( 'styles/baseplugin.css', BASEPLUGIN_FILE ), false, date( 'Ymd' ) );
 
-	}
+    }
 
-	/**
-	 * Output plugin page header
-	 *
-	 */
-	public function plugin_page_header() {
-		?>
-		<div class="icon32" id="baseplugin-icon"><br></div>
+    /**
+     * Output plugin page header
+     *
+     */
+    public function plugin_page_header() {
+        ?>
+        <div class="icon32" id="baseplugin-icon"><br></div>
 
-		<h2><?php echo sprintf( __( '%s', 'baseplugin' ), $this->name ); ?></h2>
-		<?php
-	}
+        <h2><?php echo sprintf( __( '%s', 'baseplugin' ), $this->name ); ?></h2>
+        <?php
+    }
 
-	/**
-	 * Output plugin page contents
-	 *
-	 * This is where you should output your plugin settings page.
-	 */
-	public function plugin_page_settings() {
-		?>
-		<div class="wrap">
+    /**
+     * Output plugin page contents
+     *
+     * This is where you should output your plugin settings page.
+     */
+    public function plugin_page_settings() {
+        ?>
+        <div class="wrap">
 
-			<?php $this->plugin_page_header(); ?>
+            <?php $this->plugin_page_header(); ?>
 
-			<p><?php _e( 'This is a sample plugin page.', 'baseplugin' ); ?></p>
+            <p><?php _e( 'This is a sample plugin page.', 'baseplugin' ); ?></p>
 
-		</div>
-	<?php
-	}
+        </div>
+    <?php
+    }
 
 } // Base_Plugin
 
